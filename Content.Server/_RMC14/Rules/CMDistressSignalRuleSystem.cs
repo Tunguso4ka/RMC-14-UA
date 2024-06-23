cccca60a85af48a9cd2383f058e0368c5c9f8e6a
Tunguso4ka <kiramilyavskaya@tutanota.com>
2024-06-23 11:07:04 +0300

    fix distress signal

--- Content.Server/_RMC14/Rules/CMDistressSignalRuleSystem.cs
+++ Content.Server/_RMC14/Rules/CMDistressSignalRuleSystem.cs
@@ -560,7 +560,7 @@ public sealed class CMDistressSignalRuleSystem : GameRuleSystem<CMDistressSignal
     {
         // TODO RMC14 different planet-side maps
         var mapId = _mapManager.CreateMap();
-        if (!_mapLoader.TryLoad(mapId, "/Maps/_CM14/solaris.yml", out var grids) ||
+        if (!_mapLoader.TryLoad(mapId, "/Maps/_RMC14/solaris.yml", out var grids) ||
             grids.Count == 0)
         {
             return false;
